29b92ab86e566a84f215bd3b05cc4c7314dbb738
Lee Connelly <lee@leeconnelly.dev>
2023-03-16 20:50:16 +0000

    Generate rectangles together

--- src/RectangleGenerator.ts
+++ src/RectangleGenerator.ts
@@ -18,22 +18,37 @@ export default class RectangleGenerator {
     const elapsed = timestamp - this.lastRectTime
 
     if (elapsed > 2000) {
-      // Create a new rectangle with a random height and position
-      const height = Math.floor(Math.random() * 100) + 50
-      const position = Math.floor(Math.random() * (this.canvas.height - height))
-      const rect = new Rectangle(this.canvas.width, position, 50, height, 0.5)
+      // Determine if we should generate one or two rectangles
+      const numRectsToGenerate = Math.random() < 0.5 ? 1 : 2
 
-      // Add the rectangle to the list of rectangles
-      this.rects.push(rect)
+      // Determine if the first rectangle should be attached to the top or bottom of the canvas
+      const attachToTop = Math.random() < 0.5
+
+      // Generate the first rectangle
+      const height1 = Math.floor(Math.random() * 100) + 200
+      const positionY1 = attachToTop ? 0 : this.canvas.height - height1
+      const rect1 = new Rectangle(this.canvas.width, positionY1, height1)
+
+      // If we're generating two rectangles, generate the second rectangle
+      let rect2: Rectangle | null = null
+      if (numRectsToGenerate === 2) {
+        // Determine if the second rectangle should be attached to the top or bottom of the canvas
+        const attachToTop2 = !attachToTop
+
+        // Generate the second rectangle with a heavily skewed height
+        const height2 = Math.floor(Math.random() * 300) + 50
+        const positionY2 = attachToTop2 ? 0 : this.canvas.height - height2
+        rect2 = new Rectangle(this.canvas.width, positionY2, height2)
+      }
+
+      // Add the rectangles to the list of rectangles
+      this.rects.push(rect1)
+      if (rect2) {
+        this.rects.push(rect2)
+      }
 
       // Update the last rectangle generation time
       this.lastRectTime = timestamp
-    }
-
-    // Move each rectangle to the left
-    for (let i = 0; i < this.rects.length; i++) {
-      const rect = this.rects[i]
-      rect.x -= 5
     }
 
     // Remove any rectangles that have gone off the screen
@@ -41,16 +56,8 @@ export default class RectangleGenerator {
   }
 
   update() {
-    if (this.rects.length === 0) {
-      return
-    }
-
     for (const rect of this.rects) {
       rect.update()
-    }
-
-    if (this.rects[0].x + this.rects[0].width < 0) {
-      this.rects.splice(0, 2)
     }
   }
 
