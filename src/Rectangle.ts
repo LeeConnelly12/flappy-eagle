8392a5d8cea955e360924fe7ff53032bb4ed64c7
Lee Connelly <lee@leeconnelly.dev>
2023-03-16 20:28:08 +0000

    Make rectangles slower

--- src/Rectangle.ts
+++ src/Rectangle.ts
@@ -1,23 +1,15 @@
 export default class Rectangle {
   x: number
   y: number
-  width: number
+  width = 80
   height: number
-  speed: number
+  speed = 4
   passed = false
 
-  constructor(
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    speed: number,
-  ) {
+  constructor(x: number, y: number, height: number) {
     this.x = x
     this.y = y
-    this.width = width
     this.height = height
-    this.speed = speed
   }
 
   draw(ctx: CanvasRenderingContext2D) {
