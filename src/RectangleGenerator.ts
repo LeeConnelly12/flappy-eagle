5ec8eea82d11c8e808cc36d5211281c71f63e29f
Lee Connelly <lee@leeconnelly.dev>
2023-03-18 12:51:26 +0000

    Always generate 2 rectangles

--- src/RectangleGenerator.ts
+++ src/RectangleGenerator.ts
@@ -15,34 +15,29 @@ export default class RectangleGenerator {
     const elapsed = timestamp - this.lastRectTime
 
     if (elapsed > 2000) {
-      // Determine if we should generate one or two rectangles
-      const numRectsToGenerate = Math.random() < 0.5 ? 1 : 2
-
-      // Determine if the first rectangle should be attached to the top or bottom of the canvas
-      const attachToTop = Math.random() < 0.5
-
       // Generate the first rectangle
       const height1 = Math.floor(Math.random() * 100) + 200
-      const positionY1 = attachToTop ? 0 : this.canvas.height - height1
+      const positionY1 = 0
       const rect1 = new Rectangle(this.canvas.width, positionY1, height1)
 
-      // If we're generating two rectangles, generate the second rectangle
-      let rect2: Rectangle | null = null
-      if (numRectsToGenerate === 2) {
-        // Determine if the second rectangle should be attached to the top or bottom of the canvas
-        const attachToTop2 = !attachToTop
+      // Determine the minimum gap between the two rectangles
+      const minGap = 150
 
-        // Generate the second rectangle with a heavily skewed height
-        const height2 = Math.floor(Math.random() * 300) + 50
-        const positionY2 = attachToTop2 ? 0 : this.canvas.height - height2
-        rect2 = new Rectangle(this.canvas.width, positionY2, height2)
-      }
+      // Calculate the maximum height for the second rectangle to ensure a minimum gap
+      const maxHeight2 = this.canvas.height - (positionY1 + height1) - minGap
+
+      // If the maximum height is less than the minimum height, use the minimum height instead
+      const minHeight2 = 68
+      const height2 = Math.max(
+        minHeight2,
+        Math.floor(Math.random() * maxHeight2),
+      )
+
+      const positionY2 = this.canvas.height - height2
+      const rect2 = new Rectangle(this.canvas.width, positionY2, height2)
 
       // Add the rectangles to the list of rectangles
-      this.rects.push(rect1)
-      if (rect2) {
-        this.rects.push(rect2)
-      }
+      this.rects.push(rect1, rect2)
 
       // Update the last rectangle generation time
       this.lastRectTime = timestamp
