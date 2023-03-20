73473ce791e3ad10776d05e54ad1afbeffc51ba9
Lee Connelly <lee@leeconnelly.dev>
2023-03-20 20:01:42 +0000

    Update submit form

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        green: {
+          500: '#67D242',
+          600: '#5DBC3C',
+        },
+      },
+    },
   },
   plugins: [],
 }
